02b1203860f1c2033b3bc78298c95a6e518a3cab
IvanCCO <ivan.miranda@sptech.school>
2025-05-10 23:21:03 -0300

    Header

--- src/lib/layout/components/header.tsx
+++ src/lib/layout/components/header.tsx
@@ -1,9 +1,51 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, HStack, Link, Image } from '@chakra-ui/react';
 
 export const Header = () => {
+  const navItems = [
+    { name: 'Como funciona', href: '#how-it-works' },
+    { name: 'Prêmios', href: '#prizes-section' },
+    { name: 'Indique', href: '#affiliate-form' },
+  ];
+
   return (
-    <Flex as="header" width="full" align="center">
-      <Box marginLeft="auto"></Box>
+    <Flex 
+      as="header" 
+      width="full" 
+      align="center"
+      px={{ base: 4, md: 6 }}
+      bg="white"
+    >
+      <Box>
+        <Image 
+          src="/logo-placeholder.svg" 
+          alt="Company Logo"
+          height="40px"
+        />
+      </Box>
+
+      <HStack 
+        as="nav" 
+        gap={{ base: 4, md: 6 }}
+        display={{ base: 'none', md: 'flex' }}
+        mx="auto"
+        justify="center"
+      >
+        {navItems.map((item) => (
+          <Link
+            key={item.name}
+            href={item.href}
+            fontWeight="medium"
+            color="gray.600"
+            textTransform="capitalize"
+            _hover={{
+              textDecoration: 'none',
+              color: 'blue.500',
+            }}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </HStack>
     </Flex>
   );
 };
