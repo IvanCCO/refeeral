e66a8591f544bcbc6bd795950f32c6d6408942a5
IvanCCO <ivan.miranda@sptech.school>
2025-05-15 11:55:15 -0300

    fix(utils): SEO
    SEO

--- src/app/affiliate/page.tsx
+++ src/app/affiliate/page.tsx
@@ -1,6 +1,10 @@
 import { LandingPageAffiliate } from '@/lib/pages/landing-affiliate';
-import { defaultMetadata } from '@/seo/metadata';
+import { generatePageMetadata } from '@/seo/metadata';
 
-export const metadata = defaultMetadata;
+export const metadata = generatePageMetadata(
+  'Indique a Brio para um amigo e ganhe dinheiro',
+  'Indique a Brio para um amigo e ganhe dinheiro',
+  'https://kindle2notion.s3.us-east-2.amazonaws.com/defaults/brio-link-preview.png'
+);
 
 export default LandingPageAffiliate;
